a89f467a7e5d4fdd12bd2297b44aea0e0f1c7e8c
marcosliarte <marcos.liarte.neves@gmail.com>
2023-07-31 01:41:32 -0400

    Criação das interfaces e estruturas de dados para as entidades tutor e pet

---
+++ src/models/petModel.ts
@@ -0,0 +1,23 @@
+import mongoose, {Schema, Document, model} from "mongoose";
+
+export interface Pet{
+    id: number;
+    name: string;
+    species: string;
+    carry: string;
+    weight: number;
+    date_of_birth: string;
+}
+
+export interface PetModel extends Pet, Document {}
+
+const petSchema = new Schema({
+    id: {type: Number, require: true},
+    name: {type: String, require: true},
+    species: {type: String, require: true},
+    carry: {type: String, require: true},
+    weight: {type: Number, require: true},
+    date_of_birth: {type: String, require: true}
+});
+
+export default model<PetModel>('Pet', petSchema);
---
+++ src/models/tutorModels.ts
@@ -0,0 +1,25 @@
+import mongoose, {Schema, Document, model} from "mongoose";
+
+export interface Tutor{
+    id:number;
+    name:string;
+    phone:string;
+    email:string;
+    date_of_birth:string;
+    zip_code:string;
+    pets?: Pet[];
+}
+
+export interface TutorModel extends Tutor, Document {}
+
+const tutorSchema = new Schema({
+    id: {type: Number, require: true},
+    name: {type: String, require: true},
+    phone: {type: String, require: true},
+    email: {type: String, require: true},
+    date_of_birth: {type: String, require: true}, 
+    zip_code: {type: String, require: true}
+    pets: [{type: Schema.Types.ObjectId, ref: 'Pet'}]
+});
+
+export default model<TutorModel>('Tutor', tutorSchema);
